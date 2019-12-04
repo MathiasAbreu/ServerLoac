064a7b8410f23461a76d7f42b0ce8ce445a54a5e
MathiasAbreu <mathias.trajano@ccc.ufcg.edu.br>
2019-12-04 17:59:45 -0300

    Tentando habilitar CORS

--- src/main/java/br/com/loac/server/ServerLoac/ServerLoacApplication.java
+++ src/main/java/br/com/loac/server/ServerLoac/ServerLoacApplication.java
@@ -1,10 +1,28 @@
 package br.com.loac.server.ServerLoac;
 
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ServerLoacApplication {
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerLoacApplication.class, args);
